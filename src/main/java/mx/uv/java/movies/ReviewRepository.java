0cb4d9da9ba758b65191b43797444884aac623a7
Victor Luevano <victorluevmon@gmail.com>
2024-05-10 20:20:17 -0600

    Versión 1

---
+++ src/main/java/mx/uv/java/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package mx.uv.java.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
